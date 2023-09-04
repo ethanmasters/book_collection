5af072b06664c3e6b205a2cbb633089faf485c48
ethanmasters <emasters@tamu.edu>
2023-09-04 10:25:54 -0500

    Updated code for Book Collection 1 Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    member do
+      get 'destroy_confirm', to: 'books#destroy_confirm', as: 'destroy_confirm'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "books#index"
 end
