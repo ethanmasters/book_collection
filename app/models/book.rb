0d5e5f356e8cce71b85eb6d0ba45fcbb8cf1108c
ethanmasters <emasters@tamu.edu>
2023-09-11 11:41:45 -0500

    Added updated for Book Collection 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ApplicationRecord
     validates :author, presence: true
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :published_date, presence: true
+
+    has_many :users, through: :user_books
+    has_many :user_books
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books do
     member do
-      get 'destroy_confirm', to: 'books#destroy_confirm', as: 'destroy_confirm'
+      get :delete
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "books#index"
+  root "user_books#index"
 end
